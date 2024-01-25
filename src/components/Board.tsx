7d7bede599bdcd512b1e52fb05b6cf7e0939bc16
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-01-25 14:10:58 +0530

    add columns and card

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,9 +1,33 @@
+import Column from "./Columns";
 import NewColumnForm from "./forms/NewColumnForm";
 
-export default function Board(){
+const columns = [
+    { id: 'abc', name: 'todo', index: 0 },
+    { id: 'ijk', name: 'in progress', index: 1 },
+    { id: 'xyz', name: 'done', index: 2 }
+];
+
+export type CardType = {
+    id: String;
+    name: String;
+    order: Number;
+};
+
+const cards = [
+    { id: 'dwsd', name: 'task 1', order: 0, columnId: 'abc' },
+    { id: 'fwfw', name: 'task 2', order: 1, columnId: 'ijk' },
+    { id: 'fef', name: 'task 3', order: 2, columnId: 'xyz' },
+];
+
+export default function Board() {
     return (
-        <div>
+        <div className="flex gap-4">
+            {columns.map(column => (
+                <Column
+                    {...column}
+                    cards={cards.filter(c => c.columnId === column.id)} />
+            ))}
             <NewColumnForm />
         </div>
-    )
+    );
 }
--- src/components/forms/NewColumnForm.tsx
+++ src/components/forms/NewColumnForm.tsx
@@ -1,8 +1,23 @@
+'use client'
+
+import { FormEvent } from "react";
+
 export default function NewColumnForm(){
+    
+    function handleNewColumn(ev: FormEvent){
+        ev.preventDefault();
+        const input = (ev.target as HTMLFormElement).querySelector('input');
+        const columnName = input?.value;
+        alert('New column' + columnName);
+    }
+    
     return (
-        <form>
-            <input type="text" placeholder="New column name" />
-            <button type="submit">Create column</button>
+        <form onSubmit={handleNewColumn} className="max-w-xs">
+            <label className="block mb-2">
+                <span className="text-gray-600 block">Column name:</span>
+                <input type="text" placeholder="New column name" />
+            </label>
+            <button type="submit" className="mt-2 block w-full">Create column</button>
         </form>
     )
 }
