f9ed253deeb698dbe9720975dc1f7914819b5af5
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-01-27 00:06:03 +0530

    add drag drop for cards

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,31 +1,41 @@
-import Column from "./Columns";
+'use client'
+import { use, useState } from "react";
+import Column from "./Column";
 import NewColumnForm from "./forms/NewColumnForm";
 
-const columns = [
-    { id: 'abc', name: 'todo', index: 0 },
-    { id: 'ijk', name: 'in progress', index: 1 },
-    { id: 'xyz', name: 'done', index: 2 }
+const defaultColumns = [
+    { id: 'col1', name: 'todo', index: 0 },
+    { id: 'col2', name: 'in progress', index: 1 },
+    { id: 'col3', name: 'done', index: 2 }
 ];
 
 export type CardType = {
-    id: String;
-    name: String;
-    order: Number;
+    id: string | number;
+    name: string;
+    index: number;
+    columnId: string;
 };
 
-const cards = [
-    { id: 'dwsd', name: 'task 1', order: 0, columnId: 'abc' },
-    { id: 'fwfw', name: 'task 2', order: 1, columnId: 'ijk' },
-    { id: 'fef', name: 'task 3', order: 2, columnId: 'xyz' },
+const defaultCards = [
+    { id: 'dwsd', name: 'task 1', index: 0, columnId: 'col1' },
+    { id: 'dwsk', name: 'task 4', index: 1, columnId: 'col1' },
+    { id: 'fwfw', name: 'task 2', index: 1, columnId: 'col2' },
+    { id: 'fef', name: 'task 3', index: 2, columnId: 'col3' },
 ];
 
 export default function Board() {
+    const [cards, setCards] = useState(defaultCards);
+    const [columns, setColumns] = useState(defaultColumns);
     return (
         <div className="flex gap-4">
             {columns.map(column => (
                 <Column
                     {...column}
-                    cards={cards.filter(c => c.columnId === column.id)} />
+                    setCards={setCards}
+                    cards={cards
+                        .sort((a,b) => a.index - b.index)
+                        .filter(c => c.columnId === column.id)
+                    } />
             ))}
             <NewColumnForm />
         </div>
