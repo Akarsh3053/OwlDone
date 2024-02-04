371da7ad01c6e76894b1bd02cb45fda066141c97
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-02-04 13:24:34 +0530

    add user access

--- src/app/boards/[boardId]/page.tsx
+++ src/app/boards/[boardId]/page.tsx
@@ -1,7 +1,31 @@
-export default function BoardPage(){
+'use server';
+
+import Board from "@/components/Board";
+import { liveblocksClient } from "@/lib/liveblocksClient";
+import { getUserEmail } from "@/lib/userClient";
+
+type PageProps = {
+    params: {
+        boardId: string,
+    };
+};
+export default async function BoardPage(props: PageProps){
+    const boardId = props.params.boardId;
+    const Useremail = await getUserEmail();
+    const boardInfo = await liveblocksClient.getRoom(boardId);
+    const userAccess = boardInfo.usersAccesses?.[Useremail];
+    const hasAccess = userAccess && [...userAccess].includes('room:write');
+
+    if (!hasAccess){
+        return(
+            <div>Access denied</div>
+        );
+    }
+
     return(
         <div>
-            Board pe aagya!!
+            Board: {boardInfo.metadata.boardTitle}
+            <Board />
         </div>
-    )
+    );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,14 @@ import { authOptions } from "@/lib/authOptions";
 import { getServerSession } from "next-auth";
 import LogoutButton from "./LogoutButton";
 import LoginButton from "./LoginButton";
+import Link from "next/link";
 
 export default async function Header(){
     const session = await getServerSession(authOptions)
     return(
         <header className="bg-gray-200 p-4 px-8">
             <div className="flex justify-between items-center">
-                <a href="" className="logo">OwlDone</a>
+                <Link href="/" className="logo">OwlDone</Link>
                 <div>
                     {session && (
                     <>
