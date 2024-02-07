b41eac9e70f7c6939a5fdccda34e9d8316919f8d
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-02-07 23:51:13 +0530

    add Board Settings

--- src/app/boards/[boardId]/settings/page.tsx
+++ src/app/boards/[boardId]/settings/page.tsx
@@ -1,6 +1,27 @@
-'use server'
-export default function BoardSettings(){
+'use server';
+
+import AddNewUser from "@/components/forms/AddNewUserForm";
+import { liveblocksClient } from "@/lib/liveblocksClient";
+
+type PageProps ={
+    params: {
+        boardId: string;
+    }
+}
+
+export default async function BoardSettings({params}: PageProps){
+    const {boardId} = params;
+    const boardInfo = await liveblocksClient.getRoom(boardId);
+
     return(
-        <div>Settings...</div>
+        <div>
+            <h1 className="text-2xl">Board users:</h1>
+            {Object.keys(boardInfo.usersAccesses).map(email => (
+                <div>
+                    {email}
+                </div>
+            ))}
+            <AddNewUser />
+        </div>
     )
 }
---
+++ src/components/forms/AddNewUserForm.tsx
@@ -0,0 +1,9 @@
+'use client';
+export default function AddNewUser(){
+    return(
+        <form action="" className="max-w-xs">
+            <input type="text" />
+            <button type="submit">Add User</button>
+        </form>
+    )
+}
