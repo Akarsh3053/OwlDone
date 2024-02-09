ef07966e10136e44aae921edb971cb4c7e81b979
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-02-09 16:18:38 +0530

    secure settings page

--- src/app/boards/[boardId]/settings/page.tsx
+++ src/app/boards/[boardId]/settings/page.tsx
@@ -2,6 +2,10 @@
 
 import AddNewUser from "@/components/forms/AddNewUserForm";
 import { liveblocksClient } from "@/lib/liveblocksClient";
+import { getUserEmail } from "@/lib/userClient";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 type PageProps ={
     params: {
@@ -12,9 +16,21 @@ type PageProps ={
 export default async function BoardSettings({params}: PageProps){
     const {boardId} = params;
     const boardInfo = await liveblocksClient.getRoom(boardId);
+    const userEmail = await getUserEmail();
+    if (!boardInfo.usersAccesses[userEmail]){
+        return(
+            'Access Denied'
+        )
+    }
 
     return(
         <div>
+            <Link className="inline-flex gap-1 items-center btn mb-4 max-w-xs"
+                href={`/boards/${boardId}`}>
+                <FontAwesomeIcon icon={faArrowLeft} />
+                Back to Page
+
+            </Link>
             <h1 className="text-2xl">Board users:</h1>
             <div className="mb-8">
                 {Object.keys(boardInfo.usersAccesses).map(email => (
