eded083967d8f213f8b74d851808c0437d8d1415
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-02-10 09:23:50 +0530

    Rinne Rebirth

--- src/lib/liveblocksClient.ts
+++
@@ -1,5 +0,0 @@
-import { Liveblocks } from "@liveblocks/node";
-
-export const liveblocksClient = new Liveblocks({
-    secret: process.env.LIVEBLOCKS_SECRET_KEY || '',
-  });
--- src/lib/userClient.ts
+++
@@ -1,8 +0,0 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./authOptions";
-
-export async function getUserEmail(): Promise<string> {
-    const session = await getServerSession(authOptions);
-    return session?.user?.email || '';
-    
-}
