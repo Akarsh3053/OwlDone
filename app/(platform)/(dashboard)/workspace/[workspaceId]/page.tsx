5d7265b40b71c4909966027759f851302b42e2f3
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-02-17 19:58:41 +0530

    patch new layouts

--- app/(platform)/(dashboard)/workspace/[workspaceId]/page.tsx
+++ app/(platform)/(dashboard)/workspace/[workspaceId]/page.tsx
@@ -1,19 +1,7 @@
-import { db } from "@/lib/db";
-
-import { Board } from "./board";
-import { CreateBoardForm } from "./createBoardForm";
-
 const WorkspaceIdPage = async () => {
-    const boards = await db.board.findMany();
-
     return(
-        <div className="flex flex-col space-y-4">
-            <CreateBoardForm />
-            <div className="space-y-2">
-                {boards.map((board) => (
-                    <Board key={board.id} title={board.title} id={board.id} />
-                ))}
-            </div>
+        <div className="w-full mb-20">
+            
         </div>
     );
 };
