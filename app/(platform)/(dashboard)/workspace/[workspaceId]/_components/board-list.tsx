210f79fc0930cdea5689780b1fa6299819a91edf
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-02-18 22:22:54 +0530

    add board components

---
+++ app/(platform)/(dashboard)/workspace/[workspaceId]/_components/board-list.tsx
@@ -0,0 +1,31 @@
+import { Hint } from "@/components/hint";
+import { HelpCircle, ClipboardListIcon } from "lucide-react";
+
+export const BoardList = () => {
+    return(
+        <div className="space-y-4">
+            <div className="flex items-center font-semibold text-lg text-neutral-700">
+                <ClipboardListIcon className="h-6 w-6 mr-2" />
+                Boards
+            </div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+                <div
+                    role="button"
+                    className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+                >
+                    <p className="text-sm">Create new Board</p>
+                    <span>
+                        5 remaining
+                    </span>
+                    <Hint
+                        sideOffset={40}
+                        description={`
+                            Free Workspaces can have upto Five boards, Upgrade workspace for unlimited bords.
+                        `}>
+                        <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
+                    </Hint>
+                </div> 
+            </div>
+        </div>
+    );
+};
--- app/(platform)/(dashboard)/workspace/[workspaceId]/page.tsx
+++ app/(platform)/(dashboard)/workspace/[workspaceId]/page.tsx
@@ -1,9 +1,16 @@
+import { Separator } from "@/components/ui/separator";
+
 import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
 
 const WorkspaceIdPage = async () => {
     return(
         <div className="w-full mb-20">
             <Info />
+            <Separator className="my-3" />
+            <div>
+                <BoardList />
+            </div>
         </div>
     );
 };
