fccadd34a388560cdfa28f3910418c9727f4c558
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-02-22 22:27:38 +0530

    add board title

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -1,16 +1,60 @@
 "use client";
 
+import { ElementRef, useRef, useState } from "react";
 import { Board } from "@prisma/client";
 import { Button } from "@/components/ui/button";
+import { FormInput } from "@/components/form/form-input";
 
 interface BoardTitleFormProps {
     data: Board;
   };
 
 export const BoardTitleForm = ({data}: BoardTitleFormProps) => {
+    const formRef = useRef<ElementRef<"form">>(null);
+    const inputRef = useRef<ElementRef<"input">>(null);
+    const [isEditing, setEditing] = useState(false);
+
+    const enableEditing = () => {
+        setEditing(true);
+        setTimeout(() => {
+            inputRef.current?.focus();
+            inputRef.current?.select();
+        });
+    };
+
+
+    const disableEditing = () => {
+        setEditing(false);
+    };
+
+    const onSubmit = (formData: FormData) => {
+        const title = formData.get("title") as string;
+    }
+
+    const onBlur = () => {
+        formRef.current?.requestSubmit();
+    }
+
+    if (isEditing) {
+        return(
+            <form action={onSubmit} ref={formRef} className="flex items-center gap-x-2">
+                <FormInput 
+                    ref={inputRef}
+                    id="title" 
+                    onBlur = {onBlur}
+                    defaultValue={data.title}
+                    className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent border-none"
+                />
+
+            </form>
+        );
+    }
+
     return(
         <Button
-            className="font-bold text-transform:capitalize text-lg h-auto w-auto p-1 px-2"
+            onClick={enableEditing}
+            variant={"transparent"}
+            className="font-bold capitalize text-lg h-auto w-auto p-1 px-2"
         >
             {data.title}
         </Button>
