016b596a94734fc86a1b0ea79f4bfd7b1fbb0ce6
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-02-23 17:25:04 +0530

    add list and card

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,7 +1,48 @@
-const BoardIdPage = () => {
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { ListContainer } from "./_components/list-container";
+
+interface BoardIdPageProps {
+    params: {
+      boardId: string;
+    };
+};
+
+const BoardIdPage = async ({params,}: BoardIdPageProps) => {
+    const { orgId } = auth();
+
+    if(!orgId) {
+        redirect("/select-workspace");
+    }
+
+    const lists = await db.list.findMany({
+        where: {
+           boardId: params.boardId,
+           board: {
+            orgId,
+           }, 
+        },
+        include: {
+            cards: {
+                orderBy: {
+                    order: "asc",
+                },
+            },
+        },
+        orderBy: {
+            order: "asc",
+        },
+    });
+
     return(
-        <div>
-            bhokachoda
+        <div className="p-4 h-full overflow-x-auto">
+            <ListContainer 
+                boardId = {params.boardId}
+                data = {lists}
+            />
+
         </div>
     );
 };
